11e63dcc8156887f5663459b17d2e6cf574ccd62
Haman Bagherianlemraski <hbagherianle@umass.edu>
2022-06-17 17:28:44 -0400

    Got relavant control elements

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const add_buttons = document.getElementById("add-button");
-const watched_add_button = add_buttons[0];
-const to_be_watched_add_button = add_buttons[0];
+import { MovieBuddy } from "./moviebuddy.js";
+const app = new MovieBuddy();
+// Handling watched controls
+const watched_add_button = document.getElementById("add-button")[0];
+const watched_del_button = document.getElementById("add-button")[0];
+const watched_up_button = document.getElementById("add-button");
+// Handling wish controls
+const wish_add_button = document.getElementById("add-button")[1];
+const wish_del_button = document.getElementById("add-button")[1];
+
+// Handling Recom controls
+const recom_add_button = document.getElementById("recom-button");
+
