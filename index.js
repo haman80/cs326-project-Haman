d3bda9f8c036e5b9c8f33b5177eea95d52af76f8
Haman Bagherianlemraski <hbagherianle@umass.edu>
2022-06-17 18:33:17 -0400

    Added rendering for watched list

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = new MovieBuddy();
 // Handling watched controls
 const watched_add_button = document.getElementById("add-watched-button");
 const watched_del_button = document.getElementById("delete-watched-button");
-const watched_up_button = document.getElementById("update-watched-movie");
+const watched_up_button = document.getElementById("update-watched-button");
 // Handling wish controls
 const wish_add_button = document.getElementById("add-wish-button");
 const wish_del_button = document.getElementById("delete-wish-button");
@@ -14,30 +14,30 @@ const recom_add_button = document.getElementById("recom-button");
 // Setting up event handlers for watched
 watched_add_button.addEventListener("click", function(event){
     app.add_watched(document.getElementById("adding-watched-movie").value);
-    app.render();
+    app.render(document.getElementsByClassName("table"));
 });
 watched_del_button.addEventListener("click", function(event){
     app.del_watched(document.getElementById("delete-watched-movie").value);
-    app.render();
+    app.render(document.getElementsByClassName("table"));
 });
 watched_up_button.addEventListener("click", function(event){
     app.up_watched(document.getElementById("update-watched-movie").value);
-    app.render();
+    app.render(document.getElementsByClassName("table"));
 });
 
 // Setting up event handlers for wish
 wish_add_button.addEventListener("click", function(event){
     app.add_watched(document.getElementById("adding-wish-movie").value);
-    app.render();
+    app.render(document.getElementsByClassName("table"));
 });
 wish_del_button.addEventListener("click", function(event){
     app.del_watched(document.getElementById("delete-wish-movie").value);
-    app.render();
+    app.render(document.getElementsByClassName("table"));
 });
 
 
 // Setting up event handlers for recom
 recom_add_button.addEventListener("click", function(event){
     app.up_recom();
-    app.render();
+    app.render(document.getElementsByClassName("table"));
 });
--- moviebuddy.js
+++ moviebuddy.js
@@ -16,26 +16,61 @@ export class MovieBuddy {
       
     }
   
-    render(element) {
-      element.innerHTML = '';
+    render(tables) {
+        //Render watched
+        const watched_table = tables[0];
+        const old_tbody = document.getElementById("watched_table_body");
+        const new_tbody = document.createElement('tbody');
+        new_tbody.setAttribute('id', 'watched_table_body');
+        old_tbody.parentNode.replaceChild(new_tbody, old_tbody);   
+        for (const movie of this.watched){
+            let new_row = watched_table.getElementsByTagName('tbody')[0].insertRow();
+            let new_name = new_row.insertCell();
+            let new_year = new_row.insertCell();
+            let new_rating = new_row.insertCell();
+            new_name.appendChild(document.createTextNode(movie.name));
+            new_year.appendChild(document.createTextNode(movie.year));
+            new_rating.appendChild(document.createTextNode(movie.rating));
+        }
+        //Render wish
+        const wish_table = tables[1];
+
+        //Render recom
+        const recom_table = tables[2];
+
     }
 
     // Controsl for modifying watched list
     add_watched(movie){
-        console.log(movie);
+        const info = movie.split(",");
+        this.watched.push({name:info[0] , year:info[1] , rating:info[2] });
+        console.log(this.watched);
     }
-    del_watched(movie){
-        
+    del_watched(name){
+        this.watched = this.watched.filter((elem) => {
+            if (elem.name==name){ return false;}
+            else{ return true;}
+        })
     }
-    up_watched(rating){
-
+    up_watched(movie){
+        const info = movie.split(",");
+        this.watched.forEach((elem) => {
+            if (elem.name==info[0]){ 
+                elem.rating = info[1];
+            }
+        });
+  
     }
     // Controsl for modifying wish list
     add_wish(movie){
-
+        const info = movie.split(",");
+        this.wish.push({name:info[0] , year:info[1] , rating:undefined });
     }
-    del_wish(movie){
-        
+    del_wish(name){
+        this.wish = this.wish.filter((elem) => {
+            if (elem.name==name){ return false;}
+            else{ return true;}
+        })
     }
     // Controsl for modifying recom list
     up_recom(movie){
