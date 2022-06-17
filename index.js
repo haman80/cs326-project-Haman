0ed7efd2e70de6ca2b6bcb25a91a5a524286140f
Haman Bagherianlemraski <hbagherianle@umass.edu>
2022-06-17 17:22:46 -0400

    Added moviebuddy.js which handles the logic of the application and holds the data

---
+++ index.js
@@ -0,0 +1,3 @@
+const add_buttons = document.getElementById("add-button");
+const watched_add_button = add_buttons[0];
+const to_be_watched_add_button = add_buttons[0];
---
+++ moviebuddy.js
@@ -0,0 +1,46 @@
+export class MovieBuddy {
+    constructor() {
+      // Initialize the bag.
+      this.watched = [];
+      this.wish = [];
+      this.recom = [];
+    }
+   
+    _saveState() {
+    }
+  
+    _restoreState() {
+
+    }
+    _clearState() {
+      
+    }
+  
+    render(element) {
+      element.innerHTML = '';
+    }
+
+    // Controsl for modifying watched list
+    add_watched(movie){
+
+    }
+    del_watched(movie){
+        
+    }
+    up_watched(rating){
+
+    }
+    // Controsl for modifying wish list
+    add_wish(movie){
+
+    }
+    del_wish(movie){
+        
+    }
+    // Controsl for modifying recom list
+    up_recom(movie){
+
+    }
+    
+  }
+  
